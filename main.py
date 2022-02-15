44fc651d0a9a3c84cdf474b835a20cb242d3bda9
Youngmin Park <angksflvlf52@gmail.com>
2022-02-16 08:22:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #python 3.8.6
+
 import requests
 import json
 import pandas as pd 
@@ -6,6 +7,8 @@ import datetime
 from korean_lunar_calendar import KoreanLunarCalendar
 
 if __name__ == "__main__":
+
+    now_year = '2022'
 
     # 컬럼, 행 쳐내기 
     df = pd.read_excel('data/birthday.xlsx', names=['dd', 'dept', 'name', 'birthday'])
@@ -40,16 +43,27 @@ if __name__ == "__main__":
         #print(calendar.SolarIsoFormat())
         return calendar.SolarIsoFormat()
 
-    # 양력이면 정해진 형식으로, 음력이면 양력형식으로
+    # 2022-4-4  →  2022-04-04
+    def plus_zero(date):
+        c = date.split('-')[0]
+        a = date.split('-')[1]
+        b = date.split('-')[2]
+        if len(a) == 1:
+            a = '0' + a
+        if len(b) == 1:
+            b = '0' + b
+        return c + '-' + a + '-' + b
+
+    # 양력이면 정해진 형식(xxxx-xx-xx)으로, 음력이면 양력으로 바꾼다음 형식(xxxx-xx-xx)통일
     def isLunar(x):
-        if str(x).split("(")[1].split(")")[0] == "음":
-            new_date = convert_to_korean_date(str(x).split("(")[0])
-            #print(new_date)
-            return new_date # 음력 
-        return '2022-' + str(x).split("월")[0] + '-' + str(x).split("월")[1].strip().split("일")[0] # 양력 
+        x = x.strip()
+        if str(x).split("(")[1].split(")")[0] == "음": # 음력
+            new_date_lunar = convert_to_korean_date(str(x).split("(")[0])
+            return plus_zero(new_date_lunar)  
+        new_date_solar = str(now_year) + '-' + str(x).split("월")[0] + '-' + str(x).split("월")[1].strip().split("일")[0] # 양력 
+        return plus_zero(new_date_solar)
 
     res['solar'] = res['birthday'].apply(isLunar)
-
     print(res)
 
     # dict에 집어넣기 
@@ -64,10 +78,10 @@ if __name__ == "__main__":
     # TODO: 실사용때 여기구문 사용할것 
     # 현재 날짜 추출 
     # now = datetime.datetime.now()
-    # nowDate = now.strftime('%Y-%m-%d')
+    # nowDate = str(now.strftime('%Y-%m-%d'))
     # print(nowDate)      # 2018-07-28
 
-    nowDate = '2022-4-4'
+    nowDate = '2022-04-04'
 
     # TODO
     url = 'https://lselectricdwp.webhook.office.com/webhookb2/67573c49-c614-4240-908a-bc778b3855a9@247258cc-5eb2-4fd4-9bb2-f272103f0c34/IncomingWebhook/a2f13b2f58de4f7eac433fb7f5a73e63/667762b1-4f18-4ffe-bf9c-3b0df246edc0'
@@ -94,3 +108,4 @@ if __name__ == "__main__":
     # }
     # response = requests.post(url, headers=headers, data=json.dumps(payload))
     # print(response.text.encode('utf8'))
+
