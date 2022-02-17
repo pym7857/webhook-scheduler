21ee88f5324f025a34176ffd371d43949d18c094
Youngmin Park <angksflvlf52@gmail.com>
2022-02-17 10:30:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -76,11 +76,11 @@ if __name__ == "__main__":
 
     # TODO: 실사용때 여기구문 사용할것 
     # 현재 날짜 추출 
-    # now = datetime.datetime.now()
-    # nowDate = str(now.strftime('%Y-%m-%d'))
-    # print(nowDate)      # 2018-07-28
+    now = datetime.datetime.now()
+    nowDate = str(now.strftime('%Y-%m-%d'))
+    print(nowDate)      # 2018-07-28
 
-    nowDate = '2022-03-19'
+    #nowDate = '2022-03-19'
 
     # TODO
     web_hook_url = 'https://lselectricdwp.webhook.office.com/webhookb2/67573c49-c614-4240-908a-bc778b3855a9@247258cc-5eb2-4fd4-9bb2-f272103f0c34/IncomingWebhook/a2f13b2f58de4f7eac433fb7f5a73e63/667762b1-4f18-4ffe-bf9c-3b0df246edc0'
