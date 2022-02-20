2de1a156290d5d6014c7dd28af8dcfb3d2098fd5
Youngmin Park <angksflvlf52@gmail.com>
2022-02-21 07:51:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,12 @@ if __name__ == "__main__":
     #nowDate = '2022-03-19'
 
     # TODO
-    web_hook_url = 'https://lselectricdwp.webhook.office.com/webhookb2/67573c49-c614-4240-908a-bc778b3855a9@247258cc-5eb2-4fd4-9bb2-f272103f0c34/IncomingWebhook/a2f13b2f58de4f7eac433fb7f5a73e63/667762b1-4f18-4ffe-bf9c-3b0df246edc0'
+    
+    # dx-lab 
+    web_hook_url = 'https://lselectricdwp.webhook.office.com/webhookb2/67573c49-c614-4240-908a-bc778b3855a9@247258cc-5eb2-4fd4-9bb2-f272103f0c34/IncomingWebhook/d2590810eae4454abf56af9d1579fe05/667762b1-4f18-4ffe-bf9c-3b0df246edc0'
+    
+    # playground 
+    # web_hook_url = 'https://lselectricdwp.webhook.office.com/webhookb2/67573c49-c614-4240-908a-bc778b3855a9@247258cc-5eb2-4fd4-9bb2-f272103f0c34/IncomingWebhook/a2f13b2f58de4f7eac433fb7f5a73e63/667762b1-4f18-4ffe-bf9c-3b0df246edc0'
 
     myTeamsMessage = pymsteams.connectorcard(web_hook_url)
 
